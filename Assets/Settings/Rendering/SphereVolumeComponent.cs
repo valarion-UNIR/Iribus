dfcf0147b10e8f71fc442fe9b39d11794528705b
JorgeAlonsoSuS <jorge.alonso2002@opendeusto.es>
2025-05-05 16:42:45 +0200

    PostProcessing Hecho

---
+++ Assets/Settings/Rendering/SphereVolumeComponent.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[Serializable]
+[VolumeComponentMenu("Custom/SphereVolumeComponent")]
+public class SphereVolumeComponent : VolumeComponent, IPostProcessComponent
+{
+    public ClampedFloatParameter intensity = new ClampedFloatParameter(value: 0, min: 0, max: 1, overrideState: true);
+
+    // Decide si el efecto se tiene que renderizar
+    public bool IsActive() => intensity.value > 0;
+}
